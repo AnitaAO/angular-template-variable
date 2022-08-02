8e78d13c636cb4c01f09caa603ff0cfd0f7b8d83
AnitaAO <anitaisbeta@gmail.com>
2022-08-02 10:21:52 +0100

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,16 +4,16 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   template: `
     <h1>{{ title }}</h1>
-    <p>Two was binding represented with [()] i.e [(ngModel)] known as banana in the box.<p>
-    <input [(ngModel)] ="name" (keyup.enter) = "onKeyUp()">
+    <p>Two was binding is represented with [()] i.e [(ngModel)] known as banana in the box.<p>
+    <input [(ngModel)] ="email" (keyup.enter) = "onKeyUp()">
   `,
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent  {
-  name = "John Doe";
+  email = "me@me.com";
   title = "Two way binding";
 
-  onKeyUp(email: any){
-    console.log(email)
+  onKeyUp(){
+    console.log(this.email)
   }
 }
