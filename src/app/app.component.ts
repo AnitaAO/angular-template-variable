e2b6ad7db5faaf7cb746012bb17089fa3859b824
AnitaAO <anitaisbeta@gmail.com>
2022-08-02 10:26:01 +0100

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,16 +4,15 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   template: `
     <h1>{{ title }}</h1>
-    <p>Two was binding is represented with [()] i.e [(ngModel)] known as banana in the box.<p>
-    <input [(ngModel)] ="email" (keyup.enter) = "onKeyUp()">
+    <input #email (keyup.enter) = "onKeyUp(email.value)">
   `,
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent  {
-  email = "me@me.com";
-  title = "Two way binding";
 
-  onKeyUp(){
-    console.log(this.email)
+  title = "Template Variable";
+
+  onKeyUp(email: any){
+    console.log(email)
   }
 }
