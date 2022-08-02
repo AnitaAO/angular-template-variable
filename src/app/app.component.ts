13021f2521187da2116b72f9890ba0e81d97ac6d
AnitaAO <anitaisbeta@gmail.com>
2022-08-02 10:00:45 +0100

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-    <h1>{{ title }}</>
-    <input #email (keyup.enter) = "onKeyUp(email.value)" >
+    <h1>{{ title }}</>;
+    <input #email (keyup.enter) = "onKeyUp(email.value)">
   `,
   styleUrls: [ './app.component.css' ]
 })
